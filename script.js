ee5dc2a914a6f31923d2284313ba150f44a908cf
MahimaSBisen <mahimasbisen@gmail.com>
2024-05-16 02:53:02 +0530

    buttons updated

--- script.js
+++ script.js
@@ -29,28 +29,48 @@ xhr.onreadystatechange = function () {
 const cAns = document.querySelector('#ansBut')
   cAns.addEventListener('click',function(e){
     if(e.target === cAns){
-    quiz.innerHTML = `Correct Answer`
+    quiz.innerHTML = `<h3 id="ques"> Question:  ${data[0].question.text} </h3>
+    <button class="optionAns"  id="ansBut"> A. ${data[0].correctAnswer}</button> 
+    <button class="optionAns"  id="IansBut1"> B. ${data[0].incorrectAnswers[0]}</button> 
+    <button class="optionAns"  id="IansBut2"> C. ${data[0].incorrectAnswers[1]}</button> 
+    <button class="optionAns"  id="IansBut3"> D. ${data[0].incorrectAnswers[2]}</button> 
+    <button id="butCAns"> Correct Answer </button>`
     } 
   })
 
   const wAns1 = document.querySelector('#IansBut1')
     wAns1.addEventListener('click',function(e){
       if(e.target === wAns1){
-        quiz.innerHTML = `Incorrect Answer. Try again!`
+        quiz.innerHTML = `<h3 id="ques"> Question:  ${data[0].question.text} </h3>
+        <button class="optionAns"  id="ansBut"> A. ${data[0].correctAnswer}</button> 
+        <button class="optionAns"  id="IansBut1"> B. ${data[0].incorrectAnswers[0]}</button> 
+        <button class="optionAns"  id="IansBut2"> C. ${data[0].incorrectAnswers[1]}</button> 
+        <button class="optionAns"  id="IansBut3"> D. ${data[0].incorrectAnswers[2]}</button> 
+        <button class="butWAns">Incorrect Answer</button>`
       }
   })
 
   const wAns2 = document.querySelector('#IansBut2')
     wAns2.addEventListener('click',function(e){
       if(e.target === wAns2){
-        quiz.innerHTML = `Incorrect Answer. Try again!`
+        quiz.innerHTML = `<h3 id="ques"> Question:  ${data[0].question.text} </h3>
+        <button class="optionAns"  id="ansBut"> A. ${data[0].correctAnswer}</button> 
+        <button class="optionAns"  id="IansBut1"> B. ${data[0].incorrectAnswers[0]}</button> 
+        <button class="optionAns"  id="IansBut2"> C. ${data[0].incorrectAnswers[1]}</button> 
+        <button class="optionAns"  id="IansBut3"> D. ${data[0].incorrectAnswers[2]}</button> 
+        <button class="butWAns"> Incorrect Answer</button>`
       }
   })
 
   const wAns3 = document.querySelector('#IansBut3')
     wAns3.addEventListener('click',function(e){
       if(e.target === wAns3){
-        quiz.innerHTML = `Incorrect Answer. Try again!`
+        quiz.innerHTML = `<h3 id="ques"> Question:  ${data[0].question.text} </h3>
+        <button class="optionAns"  id="ansBut"> A. ${data[0].correctAnswer}</button> 
+        <button class="optionAns"  id="IansBut1"> B. ${data[0].incorrectAnswers[0]}</button> 
+        <button class="optionAns"  id="IansBut2"> C. ${data[0].incorrectAnswers[1]}</button> 
+        <button class="optionAns"  id="IansBut3"> D. ${data[0].incorrectAnswers[2]}</button> 
+        <button class="butWAns">Incorrect Answer</button>`
       }
   })
   
@@ -60,6 +80,7 @@ const cAns = document.querySelector('#ansBut')
     count = count + 1
     if(count === 1){
       console.log("hii");
+      
     }
   })
 
